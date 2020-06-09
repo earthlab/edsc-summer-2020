9f66098f55548a7d48f792017fcde61272706d32
Leah Wasser <leah.wasser@colorado.edu>
2020-06-09 09:02:43 -0600

    vector lesson edits - WIP

---
+++ python-fundamentals/01-python-fundamentals-variables.py
@@ -0,0 +1,356 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# <img style="float: left;" src="earth-lab-logo-rgb.png" width="150" height="150">
+#
+# # Earth Data Science Corps Summer 2020
+#
+# ![Colored Bar](colored-bar.png)
+#
+# ## Introduction to Python
+
+#
+# <div class='notice--success' markdown="1">
+#
+# ## <i class="fa fa-graduation-cap" aria-hidden="true"></i> Learning Objectives
+#
+# At the end of this activity, you will be able to:
+#
+# * Explain variables are used in **Python** to store information.
+# * Use **Python** code to create variables that store single data values including numbers and text strings. 
+# * Check the data type of a variable using **Python**
+# </div>
+#  
+
+# ## What are Variables in Python?
+#
+# A variable in programming is used to store information that you want to re-use in your code. Examples of things that you may wish to save in your code include:
+#
+# * numeric values, 
+# * filenames
+# * paths 
+# * or even larger datasets such as a remote sensing image or a terrain model
+#
+# In **Python**, variables can be created without explicitly defining the type of data that it will hold (e.g. integer, text string). 
+#
+# You can create a variable in **Python** using the following syntax:
+#
+# `variable_name = value`
+
+# Try it out - run this cell
+my_variable = 5
+
+# +
+# In this cell type my_variable and run the cell!
+# -
+
+# This syntax for creating variables is the same whether you are assigning a numeric or text value to a variable. For example below you assign the variable name `a` the value of 3 which is a number:  
+#
+# `a = 3`
+#
+# Here is an example of assigning the same variable name `a` a text string value 
+#
+# `a = "a word"`.
+#
+# Notice that strings (character values) use double quotes `""` to indicate a text string value. 
+#
+#
+# <i class="fa fa-star"></i> **Data Tip:** Some programming languages require a variable to be explicitly assigned a data type when it is created. You do not need to worry about this when using `Python`, however! 
+# {: .notice--success }
+#
+#
+# ### Expressive Programming: Easy to Understand Variable Names Makes Your Code Easier to Read
+#
+# Just like you want to use <a href="https://www.earthdatascience.org/courses/intro-to-earth-data-science/open-reproducible-science/get-started-open-reproducible-science/best-practices-for-organizing-open-reproducible-science/"> expressive naming conventions (names that represent what is stored within the directory)</a> for directories on your computer, you also want to use short, clear names for your variables to make your code easier to read. Best practices for variable names include:
+#
+# When naming variables, you want to avoid: 
+#
+# * spaces in your variable names
+# * complicated wording
+# * long variable names
+# * words that do not represent what the variable contains (example: `my_variable` vs `precip_data`)
+#
+# When naming a variable you want to keep the name short but specific enough that someone reading your code can understand what it contains. It is good practice to use underscores (e.g. `boulder_precip_in`) to create multi-word variable names that provide specifics regarding the variable's content. The underscore makes the variable name easier to read and follows python <a href="https://www.python.org/dev/peps/pep-0008/#naming-conventions" target="_blank">PEP8 best practices for code readability</a>. 
+#
+#
+# <i class="fa fa-star"></i> **Data Tip:** Read more about expressive variable names and clean code <a href="https://www.earthdatascience.org/courses/intro-to-earth-data-science/write-efficient-python-code/intro-to-clean-code/expressive-variable-names-make-code-easier-to-read/">in this earth data science lesson.</a>
+# {: .notice--success }
+#
+#
+# ### Variables Are Available In Your Coding Environment Once Defined 
+#
+# A key characteristic of variables is that once you create a variable in your coding environment (that is to say you run the actual line of code that defines the variable), it is available throughout your code. So for example if you create a variable at the top of a Jupyter Notebook, the value of associated with that variable will remain the same and can be reused in cells lower down in the notebook.
+#
+# <i class="fa fa-star"></i> **Data Tip:** You can overwrite an existing variable if you create a new variable with the same name. For example if you assign `a = 5` in one cell and then assign `a = "dog"` in the next cell, the final value of `a` will be `"dog"`.
+# {: .notice--success }
+#
+# While there are occasions in which you might want to overwrite an existing variable to assign a new value to it, you want to make sure that you give variables both clear and distinct names to avoid *accidentally* overwriting variable values.  
+
+# Here you assign the variable name temperature the value 55 (a number)
+temperature = 55
+temperature
+
+# Note that you can easily reassign a variable name to contain a new value (a string / word in this case)
+temperature = "doctor"
+temperature
+
+#
+# ## About Data Types in Python
+#
+# Python data types are important to understand when coding. Below you will learn 
+# more about the different types of variables that you can store including:
+#
+# * Numbers (integers and floats)
+# * Strings (letters / characters and words)
+#
+# ## Numeric Variables in Python
+#
+# In **Python**, you can create variables to store numeric values such as  
+# integers (`int`) which represent whole numbers and floating point numbers 
+# which represent decimal values (`float`).  
+#
+# <i class="fa fa-star"></i> **Data Tip:** For more advanced math applications, you can also use variables to work with <a href="https://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex" target="_blank">complex numbers (see Python documentation for more details)</a>.
+# {: .notice--success }
+#
+# As described previously, you do not need to define which numeric type you want to use to create a variable. 
+#
+# For example, you can create a `int` variable called `boulder_precip_in`, which contains the value for the [average annual precipitation in inches (in) in Boulder, Colorado](https://psl.noaa.gov/boulder/Boulder.mm.precip.html), rounded to the nearest integer.
+
+# +
+# This is a comment line in Python
+boulder_precip_integer = 20
+
+# You can see the value of any variable by calling the variable name
+boulder_precip_integer
+# -
+
+# You can use the `type()` function to figure out the type of data stored in a 
+# variable.
+
+# What is the type of data stored in boulder_precip_in
+type(boulder_precip_integer)
+
+# If your data has decimal places associated with it, it will be stored in Python 
+# as a type: `float`. 
+
+# +
+boulder_precip_float = 20.23
+
+boulder_precip_float
+# -
+
+type(boulder_precip_float)
+
+# ## How to Store Text Variables: Strings in Python
+#
+# To create a variable containing a text string (`str`), you use quotation marks 
+# (`""`) around the value. For example: 
+#
+# `variable_name = "text"`
+#
+# While in **Python** the single quote (`''`) and the double quote (`""`) are 
+# used interchangeably (see the official <a href="https://docs.python.org/3/library/stdtypes.html#text-sequence-type-str" target="_blank">Python Docs</a> for more examples), we suggest 
+# that you use double quotes `""` to define strings (type == `str`) following 
+# `PEP8` code style for Python best practices. 
+#
+# Using quotes, you can create `str` variables that contain a single word or 
+# many words, including punctuation. Below are some examples of creating a string. 
+
+# +
+city = "Boulder"
+
+city
+
+# +
+city_state = "Boulder, CO"
+
+city_state
+
+# +
+city_description = "Boulder, CO is the home of the University of Colorado, Boulder campus."
+
+city_description
+# -
+
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Interactive Activity
+#
+# You can 
+# It's time for you to create some variables of your own! In the cell below, create the following: 
+#
+# 1. a variable called `precip_int` that stores an integer value
+# 2. a variable called `precip_float` that stores a float
+# 3. a variable called `location` that has a string as its value. 
+#
+# For these variables, use the [average annual precipitation in New York City](https://www.usclimatedata.com/climate/new-york/new-york/united-states/usny0996), which is 46.23 inches. 
+#
+# Assign this precipitation value to variables as an integer (`precip_int`) and a float (`precip_float`). Then, assign the city name `New York City` to a string variable (`location`). 
+#
+# </div>
+
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Interactive Activity - What Does Float() Do?
+#
+# Run the code in the cell below. What does the `float()` function do to the value stored in the variable named `a`?
+#
+# </div>
+
+# +
+# Activity 1: run the code below - what does the float() function do? 
+a = 75
+b = float(a)
+print(a, b)
+
+# What data type is variable b? (hint: use the type() function to find out)
+
+
+# -
+
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Interactive Activity  - Create Some Variables
+#
+# It's time for you to create some variables of your own! In the cell below, create the following: 
+#
+# 1. a variable called `precip_int` that stores an integer value representing the average annual precipitation in New York City.
+# 2. a variable called `precip_float` that stores a float value representing the average annual precipitation in New York City. 
+# 3. a variable called `location` that stores the string New York City which is the name of the location for this precipipitation value.
+#
+# HINT: you can coerce a floating point value to a integer using the `int()` function. Using int() to convert your floating point value to an integer. The variable name should be precip_int. 
+#
+# Try the code below to see how this works:
+#
+# ```
+# a = 75.643
+# b = int(a)
+# print(a, b)
+# ```
+#
+# NOTE: The [average annual precipitation in New York City as seen on this website is ](https://www.usclimatedata.com/climate/new-york/new-york/united-states/usny0996), which   46.23 inches. 
+#
+# </div>
+
+# Activity 2: Assign your variable values below this comment line!
+
+
+# The cell below includes a set of tests to see if you correctly assigned the variables in the cell above. They will provide you with feedback on each variable to ensure it was correctly assigned. 
+#
+# Be sure to run the cell below to check your variables (please do not modify the cell!).
+
+# +
+# Run this cell to ensure variables were correctly assigned
+
+import notebook_tests_fundamentals
+
+# Integer tests
+try:
+    print(notebook_tests_fundamentals.variables_test_int(precip_int))
+except NameError:
+    print("""Can't find a variable named precip_int, make sure you've correctly spelled the 
+variable name and assigned it a value!""")
+
+# Float tests
+try:
+    print(notebook_tests_fundamentals.variables_test_float(precip_float))
+except NameError:
+    print("""Can't find a variable named precip_float, make sure you've correctly spelled the 
+variable name and assigned it a value!""")
+
+# String tests
+try:
+    print(notebook_tests_fundamentals.variables_test_string(location))
+except NameError:
+    print("""Can't find a variable named location, make sure you've correctly spelled the 
+variable name and assigned it a value!""")
+# -
+
+# ## Check Variable Type
+#
+# In the activities above, you used the `type()` function to determine what type of data are stored in a variable. 
+#
+# `type(variable_name)`
+#
+# For example, you can check the type of `boulder_precip_in` after each time that a variable is created with that same name to see that the type changes from `int` to `float`. 
+
+# +
+boulder_precip_in = 21
+
+type(boulder_precip_in)
+
+# +
+boulder_precip_in = 20.68
+
+type(boulder_precip_in)
+# -
+
+# You can also check the type of the variable named `city`, which is a string (`str`) regardless of how many words (or punctuation) it contains. 
+
+# +
+city = "Boulder"
+
+type(city)
+
+# +
+city = "Boulder, CO is the home of the University of Colorado, Boulder campus."
+
+type(city)
+# -
+
+# Checking the variable `type()` let's you test what a variable contains and 
+# in turn how it can be used. 
+#
+# For example, you can actually create a `str` variable that contain numbers if you use the syntax for creating a `str` variable (e.g. `variable_name = "value"`).
+#
+# Notice below that `city_precip` is still a `str`, even though it contains a number.
+
+# +
+city_precip = "20.68"
+
+type(city_precip)
+# -
+
+# If you wanted to perform math on that variable it would not work the way 
+# you think it should. 
+
+# Multiply city_precip by 2
+city_precip*2
+
+# Thus, the value itself is not important for determining whether the variable is a numeric or string type - the syntax does this. 
+#
+# A good reminder that it is important to make sure that you are defining variables with the appropriate syntax to distinguish between numeric and string types. 
+
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Challenge - Data Types & Math
+#
+# In the cell below, run the following code which divides variable a by variable b (`a/b`). 
+#
+# ```
+# a = 5
+# b = 3
+# c = a/b
+# ```
+#
+# Use the `type()` function to determine what type of value is stored in each variable defined (a, b and c).
+#
+#
+# </div>
+
+# +
+# Run the code above in this cell. Then determin the type of each variable (a, b, c)
+
+
---
+++ python-fundamentals/04-python-fundamentals-exercise.py
@@ -0,0 +1,266 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# <img style="float: left;" src="earth-lab-logo-rgb.png" width="150" height="150">
+#
+# # Earth Data Science Corps Summer 2020
+#
+# ![Colored Bar](colored-bar.png)
+#
+# ## Python Fundamentals -- Exercises 
+#
+# Now that you have learned the basics of using **Python** it's time to test your skills. Complete the activities below. Feel free to work with a colleague or friend!
+#
+
+# %% [markdown]
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Challenge: Lists and Variables  
+#
+# The data below represent average monthly precipitation for <a href="https://www.esrl.noaa.gov/psd/boulder/Boulder.mm.precip.html" target="_blank">Boulder, Colorado provided by the U.S. National Oceanic and Atmospheric Administration (NOAA).</a> 
+#
+# Month  | Precipitation (inches) |
+# --- | --- |
+# jan | 0.70 |
+# feb | 0.75 |
+# mar | 1.85 |
+# apr | 2.93 |
+# may | 3.05 |
+# june | 2.02 |
+# july | 1.93 |
+# aug | 1.62 |
+# sept | 1.84 |
+# oct | 1.31 |
+# nov | 1.39 |
+# dec | 0.84 |
+#
+# Create two **python** `lists` as follows:
+# 1. the first list should contain the months and should be called `boulder_precip_months`
+# 2. the second list should be a list of precipitation values and should be called `boulder_precip_inches`
+#
+# Both lists should contain the data in the table above in the order they are in - in the table above!
+#
+# Here are all of the data formatted to make it easier for you to create your list! 
+#
+# `0.70, 0.75,  1.85 , 2.93, 3.05 , 2.02, 1.93, 1.62, 1.84, 1.31, 1.39, 0.84`
+#
+# And here are the months:
+#
+# `jan , feb, mar, apr, may, june, july, aug, sept, oct, nov, dec`
+#
+#
+# **HINT: that the month values should be strings and the precipitation values should be floating point numbers!**
+#
+# </div>
+
+# %%
+# Create your lists in this cell!
+
+
+# %% [markdown]
+# The cell below includes a set of tests to see if you correctly completed the activity in the cell above. They will provide you with feedback that can help you complete the activity. 
+#
+# Be sure to run the cell below to check your code (please do not modify the cell!).
+
+# %%
+# Test to make sure that the lists was assigned correctly
+
+import notebook_tests_fundamentals
+
+try:
+    print(notebook_tests_fundamentals.exercise_test_months_list(boulder_precip_months))
+except NameError:
+    print("We couldn't find any list named boulder_precip_months, make sure that you correctly assigned the list and ran the cell above.")
+
+try:
+    print(notebook_tests_fundamentals.exercise_test_precip_inches_list(boulder_precip_inches))
+except NameError:
+    print("We couldn't find any list named boulder_precip_inches, make sure that you correctly assigned the list and ran the cell above.")   
+
+# %% [markdown]
+# ### Convert Precipitation Data From Inches to mm Within the List 
+#
+# Next, convert each floating point value in the `boulder_precip_inches` to mm creating a new list variable called `boulder_precip_mm`. One way to do this is to create a copy of the old list and assign it to a new variable name - like this:
+#
+# `new_list = old_list.copy()`
+#
+# HINT: There are several efficient ways of converting your data to mm including list comprehensions. But for this exercise, you can make a copy of the `boulder_precip_inches` object using
+#
+# `boulder_precip_inches.copy()` 
+#
+# Reassign that copy to the new variable called `boulder_precip_mm`.
+#
+# Finally, you can replace each value in your new list using indexing. Refer 
+# back to the lesson on lists if you don't remember how to replace a value in 
+# a list. 
+#
+# To convert inches to millimeters, you need to multiply the inches by `25.4` 
+# (1" = 25.4 mm). To make your code more legible, you may want to create a 
+# variable at the top of your notebook that stores the conversion value of `25.4`.
+# A well named conversion variable will make your code easier to manage and 
+# easier to read.
+
+# %%
+# Add code here that creates a new list by copying the old list to create a new variable
+
+
+# Convert each item in your new boulder_precip_mm list to mm
+
+
+
+# %% [markdown]
+# The cell below includes a set of tests to see if you correctly completed the activity in the cell above. They will provide you with feedback that can help you complete the activity. 
+#
+# Be sure to run the cell below to check your code (please do not modify the cell!).
+
+# %%
+# Test to make sure that the converted list was assigned correctly
+
+try:
+    print(notebook_tests_fundamentals.exercise_test_precip_mm_list(boulder_precip_mm))
+except NameError:
+    print("We couldn't find any list named boulder_precip_mm, make sure that you correctly assigned the list and ran the cell above.")   
+
+# %% [markdown]
+# ### Create a List of Lists
+#
+# You can make a list of lists (a list which contains multiple sublists) using the following syntax:
+#
+# `list_of_lists = [list_one, list_two]'`
+#
+# In the cell below create a list called `all_boulder_data` thaht contains the 
+# `boulder_precip_months` and `boulder_precip_mm` objects as sublists
+
+# %%
+# Turn your two new lists in a single list with two sublists called all_boulder_data.
+
+
+
+
+
+# %% [markdown]
+# The cell below includes a set of tests to see if you correctly completed the activity in the cell above. They will provide you with feedback that can help you complete the activity. 
+#
+# Be sure to run the cell below to check your code (please do not modify the cell!).
+
+# %%
+# Test to make sure that the list of lists was assigned correctly
+
+try:
+    print(notebook_tests_fundamentals.exercise_test_list_of_lists(all_boulder_data))
+except NameError:
+    print("We couldn't find any list named all_boulder_data, make sure that you correctly assigned the list and ran the cell above.")   
+
+# %% [markdown]
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Challenge Your Python Skills
+#
+# Modify the following code to create a plot of your data. Be sure to use the 
+# `all_boulder_data` object for your plot:
+#
+# HINT: you will need to properly index the all_boulder_data object to access each 
+# sublist within it. 
+#
+# ```python
+# # Import necessary plot package
+# import matplotlib.pyplot as plt
+#
+# # Plot monthly precipitation values
+# fig, ax = plt.subplots(figsize=(6, 6))
+# ax.bar(listname_x_axis, 
+#        listname_y_axis, 
+#        color="red")
+# ax.set(title="Add plot title here",
+#        xlabel="Add x axis label here", 
+#        ylabel="Add y axis label here")
+# plt.show()
+# ```
+#
+# Customize this plot by completing the following tasks:
+# 1. Replace `listname_x_axis` and `listname_y_axis` with the names of the lists that you created (`boulder_precip_months` and `boulder_precip_mm` from the `all_boulder_data` list).
+# 2. Change the <a href="https://matplotlib.org/mpl_examples/color/named_colors.hires.png" target="_blank">color of the plot</a> to a blue color such as aqua.
+# 3. Update the text for the titles and axes labels. 
+# 4. Modify the values in `figsize=(6, 6)` to change the size of your plot. 
+#
+# For your titles and labels, be sure to think about the following pieces of information that could help someone easily interpret the plot:
+#
+# * geographic coverage or extent of data.
+# * duration or temporal extent of the data.
+# * what was actually measured and/or represented by the data.
+# * units of measurement.
+#
+# </div>
+
+# %% caption="This plot displays monthly average precipitation values in millimeters for Boulder, CO." label="fig:bar_boulder_monthly_avg_precip"
+# Copy/paste the code from the cell above to this cell
+# Modify values as instructed
+
+
+# %% [markdown]
+# The cell below includes a set of tests to see if you correctly completed the activity in the cell above. They will provide you with feedback that can help you complete the activity. 
+#
+# Be sure to run the cell below to check your code (please do not modify the cell!).
+
+# %%
+# This code is here to check your plot
+# Please put the code for your plot in the previous cell
+
+try:
+    print(notebook_tests_fundamentals.exercise_test_plot(ax))
+except NameError:
+    print("Could not find variable 'ax', please make sure you copy and pasted the code correctly into the cell above.")
+
+# %% [markdown]
+# <div class="notice--warning" markdown="1">
+#
+# ## <i class="fa fa-pencil-square-o" aria-hidden="true"></i> BONUS Challenge: List Comprehensions in PYthon
+#
+# Above you performed many tasks manually. Included in those manual steps was one 
+# where you converted each individual value in your list from inches to mm.
+# In Python, list comprehensions are a great way to perform operations on a sequence 
+# of values stored within a list. 
+#
+# The syntax for a list comprehension is below. 
+# Essentially what is happening below, is that Python is iterating through each value
+# in the old list (`for i in my_old_list`) and multiplying it by 2 (`i*2`). In each 
+# loop, the value `i` represents the next value in the list. IN the example below i will 
+# first be the value 1, and then 2 and finally 3 given the list values are: `[1, 2, 3]`
+#
+#
+# ```python
+# my_old_list = [1, 2, 3]
+# my_new_list = [i *2 for i in my_old_list] 
+# ```
+#
+# `my_new_list` will equal each value in `my_old_list` multiplied by 2, so `my_new_list` 
+# will be equivalent to `[2, 4, 6]`. You can run this code and see the output for yourself!
+#
+# Try to experiment with writing more efficient code. Convert your 
+# `boulder_precip_inches` list of values to a new list called 
+# `boulder_precip_mm` using a list comprehension. Used the syntax above to
+# help you create this list comprehension.
+#
+# For more information on loops and changing values within a list, you can check out <a href="https://www.earthdatascience.org/courses/intro-to-earth-data-science/write-efficient-python-code/loops/" target="_blank">Earth Lab's lesson introducing loops in Python,</a> and you can go to <a href="https://www.pythonforbeginners.com/basics/list-comprehensions-in-python" target="_blank">this more in depth explanation on list comprehensions in Python.</a>
+# </div>
+
+# %%
+# Perform your conversion here
+
+
+# %%
+
+# %%
